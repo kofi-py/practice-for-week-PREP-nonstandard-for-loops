b80440f0d8c167d51f1f76ff3277e006c57bd1d1
Kofi Adu-Boateng <pastorkofi101@gmail.com>
2024-09-30 18:37:25 -0400

    Solve problems.

--- problems.js
+++ problems.js
@@ -1,33 +1,48 @@
 function oddIndices(arr) {
     // Return an array containing all the odd indices in the array
-    // Your code here
+    for (let i=0; i < arr.length; i++) {
+        if (i % 2 === 1) {
+            console.log(arr[i])
+        }
+    }
 }
+
 
 function oddReverse(arr) {
     // Return an array containing all the odd indices starting from the end
-    // Your code here
+    for (let i=arr.length - 1; i >= 0; i--) {
+        if (i % 2 === 1) {
+            console.log(arr[i])
+        }
+    }
 }
 
 function secondPower(arr) {
     // Return an array containing all indices that are powers of 2
-    // Your code here
+    for (let i = 1; i < arr.length; i *= 2) {
+        console.log(arr[i]);
+    }
 }
+
 
 function nthPower(arr, n) {
     // Return an array containing all indices that are powers of n
-    // Your code here
+    for (let i = 1; i < arr.length; i *= n) {
+        console.log(arr[i]);
+    }
 }
 
 function firstHalf(arr) {
     // Return an array containing the first half of an array
     // Include middle index on odd length arr
-    // Your code here
+    console.log(arr.slice(0, Math.ceil(arr.length / 2)));
 }
 
 function secondHalf(arr) {
     // Return an array containing the second half of an array
     // Exclude middle index on odd length arr
-    // Your code here
+    let midIndex = Math.ceil(arr.length / 2);
+    console.log(arr.slice(midIndex));
 }
 
 module.exports = {
